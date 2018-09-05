af00f37c2d83ff72eb6fa712d6c44cdb58cb2466
lim_5258@163.com <liming_5258>
2018-09-05 11:54:01 +0800

    更换版本号

--- api-relation/src/main/java/com/merelyb/relation/controller/AccVideoRelationController.java
+++ api-relation/src/main/java/com/merelyb/relation/controller/AccVideoRelationController.java
@@ -1,7 +1,12 @@
 package com.merelyb.relation.controller;
 
+import com.merelyb.bean.ResultBean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @项目: Merelyb
@@ -14,4 +19,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("relation")
 public class AccVideoRelationController {
 
+    @RequestMapping(method = RequestMethod.POST, value = "newVideo")
+    @ResponseBody
+    public ResultBean addAccVideoRelation(HttpServletRequest request){
+        ResultBean resultBean = new ResultBean();
+        return resultBean;
+    }
 }
